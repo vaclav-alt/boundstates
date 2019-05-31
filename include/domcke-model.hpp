206c20ecb77968cd49f9dfc64410b34882078828
Vaclav Alt <vaclav.alt@seznam.cz>
2019-05-31 10:53:52 +0200

    test model

---
+++ include/domcke-model.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <cmath>
+
+#include <comptools/math.hpp>
+
+class DomckeModel {
+public:
+	struct Parameters {
+		double al = 1.25;
+		double A = 1.757;
+		double B = 1.667;
+		double C = 0.98;
+		double mu = 1.0;
+		double a = 1.96;
+		double D0 = 5.0;
+		double D1 = 3.2;
+		double R0 = 0.0;
+		double t = 0.2;
+		double Qd = 1.2;
+		double bisa = -5;
+		double bisb = 10;
+	};
+	
+	DomckeModel(Parameters p) :
+		p(p)
+	{}
+
+	double f(double E) {
+		return sqrt(0.5 * p.A / M_PI) * std::pow(E / p.B, p.al) * exp(-0.5 * E / p.B);
+	}
+
+	double g(double R) {
+		auto x = R - p.R0 + sqrt(0.25 / p.C);
+		return exp(-p.C * x*x);
+	}
+
+	double Gamma(double E) {
+		auto fE = f(E);
+		return 2 * M_PI * fE * fE;
+	}
+
+	double Vde(double E, double R) {
+		return f(E) * g(R);
+	}
+
+	double Gamma(double E, double R) {
+		return 2 * M_PI * Vde(E, R);
+	}
+
+	double Delta(double E) {
+		using comptools::math::DeltaIntegral;
+		0.5 * p.A * std::pow(1.0/p.B, 2.0*p.al) / M_PI * DeltaIntegral(E, 2.0*p.al, 1.0/p.B);
+	}
+
+	double Delta(double E, double R) {
+		auto gR = g(R);
+		return gR * gR * Delta(E);
+	}
+	
+	double Vres(double R) {
+		return V0(R) + Eres(R);
+	}
+
+	double V0(double R) {
+		auto tmp = exp(-p.a * (R - p.R0)) - 1;
+		return p.D0 * tmp * tmp;
+	}
+
+	double Vd(double R) {
+		auto tmp = exp(-p.a * (R - p.R0));
+		return p.D1 * tmp * (tmp - 2 * p.t) + p.Qd;
+	}
+
+	double EresEquation(double E, double R) {
+		return Delta(E, R) + Vd(R) - V0(R) - E;
+	}
+
+private:
+	double Eres(double R) {
+		auto eres = comptools::math::Bisection([&R,this](double E) {
+					return EresEquation(E,R);
+				}, p.bisa, p.bisb);
+		return eres;
+	}
+
+
+	Parameters p;
+};
---
+++ src/domcke.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <functional>
+#include <string>
+
+#include <comptools/grid.hpp>
+
+#include "domcke-model.hpp"
+
+using namespace comptools;
+
+typedef std::function<double(double)> FunctionType;
+
+void PrintGridFunction(Grid g, const FunctionType & f, std::string filename) {
+	std::FILE * fp = std::fopen(filename.c_str(), "w");
+	for (auto x : g) {
+		fprintf(fp, "%.10f %.10f\n", x, f(x));
+	}
+	std::fclose(fp);
+}
+
+int main() {
+	DomckeModel::Parameters p;
+	p.al = 1.25;
+	p.A = 1.757;
+	p.B = 1.667;
+	p.C = 0.98;
+	p.mu = 1.0;
+	p.a = 1.96;
+	p.D0 = 5.0;
+	p.D1 = 3.2;
+	p.R0 = 2.5;
+	p.t = 0.2;
+	p.Qd = 1.2;
+	p.bisa = -5;
+	p.bisb = 15;
+
+	auto RGrid = Grid::FromMinMaxN(1.5, 10, 1000);
+	auto RGrid2 = Grid::FromMinMaxN(2.1, 5, 1000);
+	auto EGrid = Grid::FromMinMaxN(-10, 15, 1500);
+
+
+	DomckeModel model(p);
+	PrintGridFunction(RGrid, [&model](double R) { return model.V0(R); }, "V0.dat");
+	PrintGridFunction(RGrid, [&model](double R) { return model.Vd(R); }, "Vd.dat");
+	
+	PrintGridFunction(EGrid, [&model](double E) { return model.Gamma(E, 2.5);}, "gamma.dat");
+	PrintGridFunction(EGrid, [&model](double E) { return model.Delta(E, 2.5);}, "delta.dat");
+	PrintGridFunction(EGrid, [&model](double E) { return model.EresEquation(E, 2.5);}, "eresEquation.dat");
+	PrintGridFunction(RGrid2, [&model](double R) { return model.Vres(R); }, "Vres.dat");
+	return 0;
+}
