32ce831b47768632d79de41a16a08e3ec4b17dcc
Vaclav Alt <vaclav.alt@seznam.cz>
2019-06-02 21:59:41 +0200

    use output handler and convert units to au

--- src/domcke.cpp
+++ src/domcke.cpp
@@ -6,27 +6,18 @@
 #include <comptools/grid.hpp>
 
 #include "domcke-model.hpp"
+#include "output.hpp"
 
 using namespace comptools;
 
-void PrintRow(std::FILE * fp, double x, double y) {
-		fprintf(fp, "%.10f %.10f\n", x, y);
-}
-
-void PrintRow(std::FILE * fp, double x, std::complex<double> y) {
-		fprintf(fp, "%.10f %.10f %.10f\n", x, y.real(), y.imag());
-}
-
-template<class FunctionType>
-void PrintGridFunction(Grid g, const FunctionType & f, std::string filename) {
-	std::FILE * fp = std::fopen(filename.c_str(), "w");
-	for (auto x : g) {
-		PrintRow(fp, x, f(x));
-	}
-	std::fclose(fp);
-}
 
 int main() {
+	DomckeModelUnitWrapper::UnitConversionFactors u;
+	u.DToNew = 1.8897259886;
+	u.DToOld = 1.0 / u.DToNew;
+	u.EToOld = 27.211386245988;
+	u.EToNew = 1.0 / u.EToOld;
+
 	DomckeModel::Parameters p;
 	p.al = 1.25;
 	p.A = 1.757;
@@ -36,25 +27,27 @@ int main() {
 	p.a = 1.96;
 	p.D0 = 5.0;
 	p.D1 = 3.2;
-	p.R0 = 2.5;
+	p.R0 = 2.5 * u.DToOld;
 	p.t = 0.2;
 	p.Qd = 1.2;
 	p.bisa = -50;
 	p.bisb = 20;
+
 
 	auto RGrid = Grid::FromMinMaxN(1.5, 10, 1000);
 	auto RGrid2 = Grid::FromMinMaxN(1.5, 10, 1000);
 	auto EGrid = Grid::FromMinMaxN(-10, 15, 1500);
 
 
-	DomckeModel model(p);
-	PrintGridFunction(RGrid, [&model](double R) { return model.V0(R); }, "V0.dat");
-	PrintGridFunction(RGrid, [&model](double R) { return model.Vd(R); }, "Vd.dat");
+	DomckeModelUnitWrapper model(u, p);
+	OutputHandler<BasicPrinter> output;
+	output.PrintGridFunction(RGrid, [&model](double R) { return model.V0(R); }, "V0.dat");
+	output.PrintGridFunction(RGrid, [&model](double R) { return model.Vd(R); }, "Vd.dat");
+	output.PrintGridFunction(RGrid2, [&model](double R) { return model.Vres(R); }, "Vres.dat");
+	output.PrintGridFunction(RGrid2, [&model](double R) { return model.Vloc(R); }, "Vloc.dat");
 	
-	PrintGridFunction(EGrid, [&model](double E) { return model.Gamma(E, 2.5);}, "gamma.dat");
-	PrintGridFunction(EGrid, [&model](double E) { return model.Delta(E, 2.5);}, "delta.dat");
-	PrintGridFunction(EGrid, [&model](double E) { return model.EresEquation(E, 2.5);}, "eresEquation.dat");
-	PrintGridFunction(RGrid2, [&model](double R) { return model.Vres(R); }, "Vres.dat");
-	PrintGridFunction(RGrid2, [&model](double R) { return model.Vloc(R); }, "Vloc.dat");
+	output.PrintGridFunction(EGrid, [&model](double E) { return model.Gamma(E, 2.5);}, "gamma.dat");
+	output.PrintGridFunction(EGrid, [&model](double E) { return model.Delta(E, 2.5);}, "delta.dat");
+	output.PrintGridFunction(EGrid, [&model](double E) { return model.EresEquation(E, 2.5);}, "eresEquation.dat");
 	return 0;
 }
